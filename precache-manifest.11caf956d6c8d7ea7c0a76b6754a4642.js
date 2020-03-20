9be3cff504a5b77a3094872752a6b36d09b2426b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-20 08:26:45 +0000

    Deploy jordanliu/palindrome-checker to github.com/jordanliu/palindrome-checker.git:gh-pages

---
+++ precache-manifest.11caf956d6c8d7ea7c0a76b6754a4642.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d288068e49e3b94a9eb1f8018272c4",
+    "url": "/palindrome-checker/index.html"
+  },
+  {
+    "revision": "54dc2af3a8e44b64af08",
+    "url": "/palindrome-checker/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "7d57c7162c10951ddb32",
+    "url": "/palindrome-checker/static/css/main.26ef57ce.chunk.css"
+  },
+  {
+    "revision": "54dc2af3a8e44b64af08",
+    "url": "/palindrome-checker/static/js/2.46f78513.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/palindrome-checker/static/js/2.46f78513.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d57c7162c10951ddb32",
+    "url": "/palindrome-checker/static/js/main.0cdd9681.chunk.js"
+  },
+  {
+    "revision": "fa6a6388cf8be4c2adc6",
+    "url": "/palindrome-checker/static/js/runtime-main.c0e15dbb.js"
+  }
+]);
